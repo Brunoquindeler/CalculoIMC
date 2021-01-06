2ccfb0aedf819c5185af648f9c8df899088fc30f
Bruno Quindeler <21955104+Brunoquindeler@users.noreply.github.com>
2021-01-06 20:13:04 -0300

    Update Principal.java

--- CalculaIMC/src/Principal.java
+++ CalculaIMC/src/Principal.java
@@ -1,19 +1,19 @@
-
-public class Principal {
-
-	public static void main(String[] args) {
-		Paciente p1 = new Paciente(1.70, 72);
-		Paciente p2 = new Paciente(1.90, 115);
-		Paciente p3 = new Paciente(1.30, 65);
+class Principal {
+  public static void main(String[] args) {
+   		Paciente p1 = new Paciente(1.70, 72, "José");
+		Paciente p2 = new Paciente(1.90, 115, "Joãozinho");
+		Paciente p3 = new Paciente(1.30, 65, "Maria");
 		
+		System.out.println("Nome: "+p1.nome);
 		System.out.println("IMC: "+String.format("%.2f",p1.calcularIMC()));
-		System.out.println(p1.diagnostico());
+		System.out.println(p1.diagnostico()+"\n");
 		
+		System.out.println("Nome: "+p2.nome);
 		System.out.println("IMC: "+String.format("%.2f",p2.calcularIMC()));
-		System.out.println(p2.diagnostico());
+		System.out.println(p2.diagnostico()+"\n");
 		
+		System.out.println("Nome: "+p3.nome);
 		System.out.println("IMC: "+String.format("%.2f",p3.calcularIMC()));
-		System.out.println(p3.diagnostico());
-	}
-
+		System.out.println(p3.diagnostico()+"\n");
+  }
 }
